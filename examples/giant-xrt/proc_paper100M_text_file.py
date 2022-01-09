830235ec9c3774737edca4be26be4cff64126ee8
Fanjin Zhang <zfjsail@163.com>
2022-01-09 12:18:20 +0800

    fix and test

--- examples/giant-xrt/proc_paper100M_text_file.py
+++ examples/giant-xrt/proc_paper100M_text_file.py
@@ -3,18 +3,21 @@ import gzip
 import transformers
 import logging
 
-logging.getLogger(transformers.__name__).setLevel(logging.WARNING)
-
-LOGGER = logging.getLogger(__name__)
+# logging.getLogger(transformers.__name__).setLevel(logging.WARNING)
+# LOGGER = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s')  # include timestamp
 
 
 mid_to_title = {}
 with open("/sas/zfj/scp/paperinfo/idx_title.tsv") as rf:
     for i, line in enumerate(rf):
         if i % 10000 == 0:
-            LOGGER.info("line %d", i)
+            logger.info("line %d", i)
         items = line.strip().split("\t")
         mid_to_title[int(items[0])] = items[1]
+        if i > 10000:
+            break
 
 out_dir = "/sas/zfj/projects/network-embedding/pecos-mine-1/examples/giant-xrt/proc_data_xrt/ogbn-papers100M/"
 os.makedirs(out_dir, exist_ok=True)
@@ -22,7 +25,7 @@ wf = open(os.path.join(out_dir, "X.all.txt"))
 with gzip.open("/home/zfj/projects/network-embedding/ogb/examples/nodeproppred/papers100M/dataset/ogbn_papers100M/mapping/nodeidx2paperid.csv.gz") as fin:
     for i, line in enumerate(fin):
         if i % 10000 == 0:
-            LOGGER.info("write line %d", i)
+            logger.info("write line %d", i)
         if i == 0:
             continue
         items = line.strip().split(",")
@@ -31,4 +34,4 @@ with gzip.open("/home/zfj/projects/network-embedding/ogb/examples/nodeproppred/p
         cur_text = mid_to_title.get(mid, "")
         wf.write(cur_text + "\n")
 wf.close()
-LOGGER.info("done")
+logger.info("done")
